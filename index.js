0a85536be04582dc2782b8794cbbf8efb2c1a825
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2022-05-15 20:03:14 +0600

    mongo added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-require("dotenv").config;
+require("dotenv").config();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pyb26.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 
 app.get("/", (req, res) => {
   res.send("hello world");
